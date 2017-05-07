76a27b4b8002a4f4bc9cbb5f2d526bfa633a6dfe
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2017-05-07 12:58:48 -0600

    added response_message test case

--- test/test_mercado_bitcoin_j.ml
+++ test/test_mercado_bitcoin_j.ml
@@ -14,3 +14,24 @@ let (_test_message:unit) =
       ; event_code = 7000
       ; msg_content = "Manutenção programada para 2015-DEZ-25, janela de até 2 horas, a partir das 14hs. O sistema estará indisponível durante esse período."}
 ;;
+
+let (_test_response_message:unit) =
+  json_file_assert
+    ~file:"test/response_message_sample.json"
+    ~transform:Mercado_bitcoin_j.system_messages_response_of_string
+    ~expected:
+      (let open Mercado_bitcoin_j in
+       { response_data =
+           { messages =
+               [{msg_date = 1453827748
+                ; level = "INFO"
+                ; event_code = 7000
+                ; msg_content = "Manutenção programada para 2015-DEZ-25, janela de até 2 horas, a partir das 14hs. O sistema estará indisponível durante esse período."
+                }; {
+                  msg_date=1453827748
+                ; level = "INFO"
+                ; event_code = 7002
+                ; msg_content ="Novo filtro de datas disponível para o método *list_orders*. Veja mais detalhes em https://www.mercadobitcoin.com.br/trade-api/."}]}
+       ; status_code = 100
+       ; server_unix_timestamp = 1453827748
+       ; error_message = None})
