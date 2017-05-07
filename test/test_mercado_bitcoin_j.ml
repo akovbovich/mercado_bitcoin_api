e7eb2734164ac0073651eab6188ce6fb64ef2d56
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2017-05-07 16:19:57 -0600

    open module in test_mercado_bitcoin_j

--- test/test_mercado_bitcoin_j.ml
+++ test/test_mercado_bitcoin_j.ml
@@ -1,5 +1,7 @@
 open Core.Std
 
+open Mercado_bitcoin_j
+
 let json_file_assert ~(file:string) ~transform ~expected =
   let json=In_channel.read_all file in
   assert ((transform json) = expected)
@@ -7,9 +9,9 @@ let json_file_assert ~(file:string) ~transform ~expected =
 let (_test_message:unit) =
   json_file_assert
     ~file:"test/message_sample.json"
-    ~transform:Mercado_bitcoin_j.system_message_of_string
+    ~transform:system_message_of_string
     ~expected:
-      {Mercado_bitcoin_j.msg_date = 1453827748
+      {msg_date = 1453827748
       ; level = "INFO"
       ; event_code = 7000
       ; msg_content = "Manutenção programada para 2015-DEZ-25, janela de até 2 horas, a partir das 14hs. O sistema estará indisponível durante esse período."}
@@ -18,7 +20,7 @@ let (_test_message:unit) =
 let (_test_response_message:unit) =
   json_file_assert
     ~file:"test/response_message_sample.json"
-    ~transform:Mercado_bitcoin_j.system_messages_response_of_string
+    ~transform:system_messages_response_of_string
     ~expected:
       (let open Mercado_bitcoin_j in
        { response_data =
