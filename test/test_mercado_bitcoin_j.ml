422cbd4fc333e864f6917c94ecd2b21527f7387f
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2017-05-07 10:28:32 -0600

    First test case: message is parsed correctly

---
+++ test/test_mercado_bitcoin_j.ml
@@ -0,0 +1,15 @@
+open Core.Std
+
+let json_file_assert ~(file:string) ~transform ~expected =
+  let json=In_channel.read_all file in
+  assert ((transform json) = expected)
+
+let (_test_message:unit) =
+  json_file_assert
+    ~file:"test/message_sample.json"
+    ~transform:Mercado_bitcoin_j.system_message_of_string
+    ~expected:
+      {Mercado_bitcoin_j.msg_date = 1453827748
+      ; level = "INFO"
+      ; event_code = 7000
+      ;msg_content = "Manutenção programada"}
