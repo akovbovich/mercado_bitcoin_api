51db001f84d9949b973fed2bf6bcf55c75a82faa
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2017-05-08 22:16:07 -0400

    factoring out functionality for requests

--- mercado_bitcoin.ml
+++ mercado_bitcoin.ml
@@ -14,11 +14,24 @@ let create_handler ~tapi_id ~tapi_secret ~pair =
   { handler= Mercado_bitcoin_http.request ~tapi_id ~tapi_secret
   ; pair}
 
-let list_system_messages ?level t =
-  let params = Option.map level ~f:(fun l -> ["level",l]) in 
+
+let param_of_option_list (l: (string * string option) list)
+  : (string * string) list =
+  List.bind l (fun (k, mv) ->
+      match mv with
+      | None -> []
+      | Some v -> [k,v])
+
+let request t ~mb_method ~params ~f=
+  let params = param_of_option_list params in
   t.handler
-    ~mb_method:"list_system_messages"
-    ?params
+    ~mb_method
+    ~params
     () >>= fun (resp, body) ->
-  (Cohttp_lwt_body.to_string body) >|=
-  Mercado_bitcoin_j.system_messages_response_of_string
+  (Cohttp_lwt_body.to_string body) >|= f
+
+let list_system_messages ?level t =
+  request t
+    ~mb_method:"list_system_messages"
+    ~params:["level",level]
+    ~f:Mercado_bitcoin_j.system_messages_response_of_string
